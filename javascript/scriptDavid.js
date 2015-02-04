a65da4d5a42e948aea0918ce8feb38138d44602d
Davilink <jedi_david_20@hotmail.com>
2015-02-04 13:41:41 -0500

    Mise à jour des modifs
    Mise à jours du fichiers

--- javascript/scriptDavid.js
+++ javascript/scriptDavid.js
@@ -1,9 +1,21 @@
 //Permettre de connaitre si une partie est en cours
 //La partie peut être en cours même si elle est sur pause
 isPartieEnCours = false;
+function nonImplemente(fonctionAppelante) {
+  if(fonctionAppelante) {
+    message = function () {
+      console.log("Nom implémenté pour le moment", fonctionAppelante);
+    }
+    return message;
+  }
+console.log("Nom implémenté pour le moment");
+}
 
-if(!togglePause)
-  togglePause = function () {};
+if(typeof togglePause == 'undefined')
+  togglePause = nonImplemente('togglePause');
+
+if(typeof genererCible == 'undefined')
+   genererCible = nonImplemente('genererCible');
 
 function nouvellePartie() {
   if(isPartieEnCours) {
@@ -15,6 +27,7 @@ function nouvellePartie() {
     //Sinon le reste de la fonction sera exécuté
     if(!rep)
       return;
+    togglePause();
   }
     
   cibles = new Array();
@@ -38,7 +51,20 @@ function dessinerReussite() {
 }
 
 function dessinerEchec() {
+  var cx, cy;
+  var oMessage = {};
+  oMessage.titre.texte = "Fin de partie";
+  oMessage.titre.largeur = 0;
+  oMessage.message.texte = "Échec";
+  oMessage.message.largeur = 0;
   
+  oCtx.font = "20px ArchitectsDaughter";
+  oCtx.fillColor = 'rgb(255, 217, 5)';
+  oMessage.titre.largeur = oCtx.measureText(oMessage.titre.texte);
+  oMessage.message.largeur = oCtx.measureText(oMessage.message.texte);
+  
+  
+  oCtx.fillText('Échec', 50, 50);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
