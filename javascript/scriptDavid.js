bcd391f6f07f06211bedeb3109db257b01c54ae8
Davilink <jedi_david_20@hotmail.com>
2015-02-04 08:59:59 -0500

    Modification et ajout de fonction
    + fonction NouvellePartie (ScriptDavid)
    + Viseur sur le canvas (voir CSS)
    + Ajout du bouton pour demarrer la partie

--- javascript/scriptDavid.js
+++ javascript/scriptDavid.js
@@ -1,7 +1,46 @@
+//Permettre de connaitre si une partie est en cours
+//La partie peut être en cours même si elle est sur pause
+isPartieEnCours = false;
+
+if(!togglePause)
+  togglePause = function () {};
+
 function nouvellePartie() {
+  if(isPartieEnCours) {
+    //On met le jeux en pause
+    togglePause();
+    rep = confirm('Une partie est en cours. Désirez vous continuez quand même et mettre fin à cette partie ?');
+    //Si l'utilisateur clique sur annulez ou sur le x (pour fermer la fenêtre),
+    //alors on sort de la fonction avec l'instruction return;
+    //Sinon le reste de la fonction sera exécuté
+    if(!rep)
+      return;
+  }
+    
   cibles = new Array();
   score = 0;
   qteCibleManque = 0;
   qteCibleInterdit = 0;
+  isPlay = false;
+  isPartieEnCours = true;
   genererCible();
 }
+
+function finPartie(reussi) {
+  if(reussi)
+    dessinerReussite();
+  else
+    dessinerEchec();
+}
+
+function dessinerReussite() {
+  
+}
+
+function dessinerEchec() {
+  
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#btn_nouvellePartie').addEventListener('click', nouvellePartie);
+});
