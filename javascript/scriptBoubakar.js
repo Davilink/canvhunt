fb5a1fe1a4db16649107429ecceaac6b35d42c7e
boubakar <maguiragaboubakar@yahoo.fr>
2015-02-09 10:39:10 -0500

    Update scriptBoubakar.js

--- javascript/scriptBoubakar.js
+++ javascript/scriptBoubakar.js
@@ -1,46 +1,46 @@
-//Positionnement de la souris sur la cible et gestion du clic
+/*Positionnement de la souris sur la cible et gestion du clic
 
 var position = document.getElementById('canvHunt');
 var context = position.getContext('2d');
 var souris;
-
-    position.addEventListener('click',function gererClic(event) {
+*/
+  
         var x = event.clientX;
         var y = event.clientY;
 
         //atteindre la cible a diffents endroits
 
-
+function gererClic{
        for(var i=0; i<cibles.length; i++){
         if(x>cible.x && y>cible.y) {
             return false;
-        }else if(x==cible.x && y==cible.y){
+        }else if(x==cible.x + cible.longueur && y==cible.y + cible.hauteur){
 
                score++;
            }if(score>=MAX_SCORE){
-           alert('fin de partie');
+           FinPartie();
         }
 
         }
-    }, true);
+}    
     
         
 
-//test sur un carre immobile: si le chasseur clicke sur un element du tableau(exmple: le carre),le score augmente de 1...
+/*test sur un carre immobile: si le chasseur clicke sur un element du tableau(exmple: le carre),le score augmente de 1...
     context.strokeRect(0, 0, 45, 45);
     context.stroke();
-
+*/
 
 //pause en cours du game
 function togglePlay(){
 
 if(isPlay){
     clearInterval(t);
-    document.getElementById('canvHunt').removeEventlistener('click', gererClic(event));
+    document.getElementById('canvHunt').removeEventlistener('click', gererClic);
     }else{
         t=setInterval(deplacerCible,1);
         
-        document.getElementById('canvHunt').addEventlistener('click', gererClic(event));
+        document.getElementById('canvHunt').addEventlistener('click', gererClic);
     }
     isPlay=!isPlay;
 }
