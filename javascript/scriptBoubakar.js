ddf878c5867b4422b8bd65ab062d677865092ae9
Davilink <jedi_david_20@hotmail.com>
2015-02-09 11:09:18 -0500

    Correcion du script de boubakar

--- javascript/scriptBoubakar.js
+++ javascript/scriptBoubakar.js
@@ -1,14 +1,3 @@
-/*Positionnement de la souris sur la cible et gestion du clic
-
-var position = document.getElementById('canvHunt');
-var context = position.getContext('2d');
-var souris;
-*/
-  
-      
-
-        //atteindre la cible a diffents endroits
-
 function gererClic(event){
          var x = event.clientX;
         var y = event.clientY;
@@ -17,6 +6,7 @@ function gererClic(event){
          if(x > cibles[i].x && x < cibles[i].x + cibles[i].longueur && y > cibles[i].y && y < cibles[i].y + cibles[i].hauteur){
 
                score++;
+			   console.log(score);
            }
 
         }
@@ -25,26 +15,17 @@ function gererClic(event){
            FinPartie();
         }
 }    
-    
-        
-
-/*test sur un carre immobile: si le chasseur clicke sur un element du tableau(exmple: le carre),le score augmente de 1...
-    context.strokeRect(0, 0, 45, 45);
-    context.stroke();
-*/
 
 //pause en cours du game
 function togglePlay(){
 
 if(isPlay){
     clearInterval(t);
-    document.getElementById('canvHunt').removeEventlistener('click', gererClic);
+    oCanvas.removeEventListener('click', gererClic);
     }else{
-        t=setInterval(deplacerCible,1);
+        t=setInterval(deplacerCible,1000);
         
-        document.getElementById('canvHunt').addEventlistener('click', gererClic);
+        oCanvas.addEventListener('click', gererClic);
     }
     isPlay=!isPlay;
 }
-
-
